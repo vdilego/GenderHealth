b52ddbd626a549d6017a41656f0e4447c7479d25
Di Lego Goncalves <Vanessa.DiLego@oeaw.ac.at>
2023-09-13 17:58:17 +0200

    comparing now with gender indices

---
+++ 4_GenderIndices.R
@@ -0,0 +1,4 @@
+# using the indicator to rank countries and then use our decomposition to illustrate
+# the effect
+
+# using data from Gender Equality Index scores, SOURCE: European Institute for Gender Equality (EIGE)
